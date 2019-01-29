cb1cc47c97fabd4fa18ec23c0e35797d41c48b25
Lucashsmello <lucashsmello@gmail.com>
2019-01-29 10:06:17 +0000

    Notify network of gimx-webapi on startup

--- src/registerService.py
+++ src/registerService.py
@@ -4,15 +4,25 @@ from time import sleep
 from threading import Thread
 
 DOLISTEN=True
+UDP_PORT=51915
 
 def unregisterService():
 	global DOLISTEN
 	DOLISTEN=False
 
+def sendHIMessage(address):
+	global UDP_PORT
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+	if(address=='<broadcast>'):
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+	sock.sendto("megimxservice", (address, UDP_PORT))
+	sock.close()
+
 def listenBroadCast():
-	global DOLISTEN
+	global DOLISTEN,UDP_PORT
 	DOLISTEN=True
-	UDP_PORT=51915
+	
+	sendHIMessage('<broadcast>')
 	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 	client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 	client.bind(("", UDP_PORT))
@@ -20,8 +30,7 @@ def listenBroadCast():
 		data, addr = client.recvfrom(1024)
 		#print("received message from %s: %s" % (addr,data))
 		if(data=="whoGIMXAPISERVICE"):
-			sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-			sock.sendto("megimxservice", (addr[0], UDP_PORT))
+			sendHIMessage(addr[0])
 	client.close()
 
 def registerService():
